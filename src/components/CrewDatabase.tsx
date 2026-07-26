c3df9b4bdeb3f871ab3845a4f2100a73ebf6a3b8
kaynguyen1308 <kaynguyen1308@gmail.com>
2026-07-26 17:24:43 +0700

    Updated CrewDatabase.tsx

--- src/components/CrewDatabase.tsx
+++ src/components/CrewDatabase.tsx
@@ -236,7 +236,7 @@ function useCrewEngine(
       trigger: section,
       start: 'top top',
       end: 'bottom bottom',
-      scrub: 0.8,
+      scrub: 0.1,
       onUpdate: (self) => {
         const p = self.progress;
         // Camera reaches David at P_CAMERA, then holds for David's dwell.
